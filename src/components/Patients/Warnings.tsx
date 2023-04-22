27aa1183cacc17f561187d0330df1529b4a9791c
peterjanos <janosp207@gmail.com>
2023-04-22 16:24:19 +0200

    add title

--- src/components/Patients/Warnings.tsx
+++ src/components/Patients/Warnings.tsx
@@ -1,5 +1,5 @@
 import Warning from '@/classes/Warning'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import WarningBox from './InfoBoxes/WarningBox'
 
 type Props = {
@@ -10,10 +10,13 @@ const Warnings = ({ warnings }: Props): JSX.Element => {
   if (!warnings) return <></>
 
   return (
-    <Box display='flex' flexDirection='row' gap={3} pl={3} pt={3}> 
-      {warnings.map(warning => (
-        <WarningBox key={warning.type} warning={warning}/>
-      ))}
+    <Box pl={3} pt={3}>
+      <Typography variant='h6' mb={2}>Based on data from last 30 days</Typography>
+      <Box display='flex' flexDirection='row' gap={3}> 
+        {warnings.map(warning => (
+          <WarningBox key={warning.type} warning={warning}/>
+        ))}
+      </Box>
     </Box>
   )
 }
